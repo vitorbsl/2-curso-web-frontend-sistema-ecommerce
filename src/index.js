a27c64d7ed995e79ff4588f639820295d9da306c
Julli Mayanne <jullimayanne9@gmail.com>
2024-10-07 19:14:59 -0300

    feat: add src

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+console.log("Hello World");
+
 // Lista de produtos disponíveis com nome e preço
 const produtosDisponiveis = [
-    { nome: "Camisa", preco: 50.00 },
-    { nome: "Calça", preco: 100.00 },
-    { nome: "Sapato", preco: 150.00 },
-    { nome: "Boné", preco: 25.00 }
+  { nome: "Camisa", preco: 50.0 },
+  { nome: "Calça", preco: 100.0 },
+  { nome: "Sapato", preco: 150.0 },
+  { nome: "Boné", preco: 25.0 },
 ];
 
 // Carrinho de compras como um array de objetos
 let carrinho = [];
-
